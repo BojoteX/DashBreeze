e26c5c8020e44df8c10e18654097f959100d0f82
Jesus Altuve <jaltuve@gmail.com>
2023-06-15 17:19:28 -0400

    Add project files.

---
+++ DeviceAutoDetect.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Bojote.DashBreeze
+{
+    public class DeviceAutoDetect
+    {
+        public static event Action<SerialConnection> ReadySerialPortFound;
+        readonly SettingsControl settingsControl = new SettingsControl();
+
+        public async Task CheckSerialPort(MainSettings Settings)
+        {
+            int BaudRate = int.Parse(Settings.SelectedBaudRate);
+
+            int timeoutMilliseconds = 1000;  // 5 seconds timeout
+            string[] actualPortNames = SerialPort.GetPortNames();
+
+            var tasks = new List<Task>();
+
+            foreach (var portName in actualPortNames)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    using (SerialConnection SC = new SerialConnection())
+                    {
+                        try
+                        {
+                            if (SC == null)
+                                return;
+
+                            await SC.Connect(portName, BaudRate, ResetCon: false);
+
+                            if (!SC.IsConnected)
+                                return;
+
+                            SimHub.Logging.Current.Info($"Opened {portName}");
+
+                            string SendString = Main.Constants.HandShakeSnd;
+                            string HandShakeString = Main.Constants.HandShakeRcv;
+
+                            byte[] command255 = { 255 };
+                            SC.SerialPort.Write(command255, 0, 1);
+                            SC.SerialPort.WriteLine(SendString);
+
+                            SimHub.Logging.Current.Info($"Sent the string {SendString} preceded by the Byte 255");
+
+                            StringBuilder response = new StringBuilder();
+                            byte[] buffer = new byte[1024];
+                            int bytesRead;
+                            bool handshakeReceived = false;
+
+                            using (var cts = new CancellationTokenSource(timeoutMilliseconds))
+                            {
+                                try
+                                {
+                                    while (!handshakeReceived)
+                                    {
+                                        if (SC.SerialPort.BytesToRead > 0)
+                                        {
+                                            bytesRead = SC.SerialPort.Read(buffer, 0, buffer.Length);
+                                            response.Append(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+
+                                            if (response.ToString().Contains(HandShakeString))
+                                                handshakeReceived = true;
+
+                                            SimHub.Logging.Current.Info($"I'm reading data now");
+                                        }
+
+                                        cts.Token.ThrowIfCancellationRequested();
+                                    }
+                                }
+                                catch (OperationCanceledException)
+                                {
+                                    // Cancellation request has been made, handle it
+                                    SimHub.Logging.Current.Info($"Operation on port {portName} was cancelled.");
+                                }
+                            }
+
+                            if (handshakeReceived)
+                            {
+                                ReadySerialPortFound?.Invoke(SC);
+                            }
+                        }
+                        catch (UnauthorizedAccessException ex)
+                        {
+                            SimHub.Logging.Current.Error($"Access to the port '{portName}' is denied. {ex.Message}");
+                            // Handle the exception here...
+                        }
+                        catch (IOException ex)
+                        {
+                            SimHub.Logging.Current.Error($"Error opening or writing to {portName}: {ex.Message}");
+                        }
+
+                        catch (InvalidOperationException ex)
+                        {
+                            SimHub.Logging.Current.Error($"Error opening or writing to {portName}: {ex.Message}");
+                        }
+
+                        catch (ArgumentException ex)
+                        {
+                            SimHub.Logging.Current.Error($"Error opening or writing to {portName}: {ex.Message}");
+                        }
+
+                        finally
+                        {
+                            SimHub.Logging.Current.Info($"Closed {portName}");
+                            await Application.Current.Dispatcher.InvokeAsync(() => settingsControl.OutputMsg($"Closed {portName}"));
+                        }
+                    }
+                }));
+            }
+            await Task.WhenAll(tasks);  // Wait for all tasks to complete
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DashBreeze")]
+[assembly: AssemblyDescription("Controls fans based on your simulator's speed telemetry data to simulate the feeling of wind rushing against your face")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bojote")]
+[assembly: AssemblyProduct("DashBreeze")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed9ba02a-13fc-4cfd-97ce-08614dbaaa35")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
